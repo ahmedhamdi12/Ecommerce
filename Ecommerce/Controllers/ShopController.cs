5461bcf3e857966f436b5397c995815cc7911268
ahmedhamdi12 <ahmedhbarakat123@gmail.com>
2026-03-27 04:49:55 +0200

    Add search + filter + pagination

--- Ecommerce/Controllers/ShopController.cs
+++ Ecommerce/Controllers/ShopController.cs
@@ -12,10 +12,36 @@ namespace Ecommerce.Controllers
         public ShopController(ApplicationDbContext context){
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string search, int? categoryId, int page=1)
         {
-            var products = _context.Products.Include(p => p.Category).ToList();
-            return View(products);
+            int pageSize = 6;
+            var products = _context.Products.Include(p => p.Category).AsQueryable();
+
+            //search
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name.Contains(search));
+            }
+
+            //filter
+            if (categoryId.HasValue)
+            {
+                products = products.Where(p => p.CategoryId == categoryId);
+            }
+
+            //pagination
+            int totalItems = products.Count();
+
+            var pagedProducts = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages= (int)Math.Ceiling((double)totalItems / pageSize);
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(pagedProducts);
         }
 
         public IActionResult Details(int id)
