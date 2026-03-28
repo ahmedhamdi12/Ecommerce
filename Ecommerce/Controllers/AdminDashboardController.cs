f5728611eaa37bf2885de5a29dc39e8af324419c
ahmedhamdi12 <ahmedhbarakat123@gmail.com>
2026-03-28 19:08:40 +0200

    create admin dashboard

---
+++ Ecommerce/Controllers/AdminDashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminDashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var totalOrders = _context.Orders.Count();
+            var totalRevenue = _context.Orders.Sum(o => o.Total);
+            var totalUsers = _context.Users.Count();
+
+            ViewBag.TotalOrders = totalOrders;
+            ViewBag.TotalUsers = totalUsers;
+            ViewBag.TotalRevenue = totalRevenue;
+            return View();
+        }
+    }
+}
